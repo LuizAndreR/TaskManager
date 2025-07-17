e81579f9df928001ef458f77391e56eb84f7a450
Luiz André <luizandre12042004@gmail.com>
2025-07-17 16:03:59 -0300

    Adicionar arquivos de projeto.

---
+++ src/TaskManager.Domain/Entities/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManager.Domain.Entities;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string SenhaHash { get; set; }
+}
---
+++ src/TaskManager.Infrastructure/Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Data.Mappings;
+
+internal class UsuarioMap : IEntityTypeConfiguration<Usuario>
+{
+    public void Configure(EntityTypeBuilder<Usuario> builder)
+    {
+        builder.ToTable("Usuarios");
+
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Nome)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(u => u.SenhaHash)
+            .IsRequired();
+    }
+}
